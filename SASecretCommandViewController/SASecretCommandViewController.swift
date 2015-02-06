674e1ed8386b64f530360f6ba437094c9425b662
Taiki Suzuki <s1180183@gmail.com>
2015-02-07 00:10:37 +0900

    add SASecretCommandViewController

---
+++ SASecretCommandViewController/SASecretCommandViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SASecretCommandViewController.swift
+//  SASecretCommandViewController
+//
+//  Created by 鈴木大貴 on 2015/02/07.
+//  Copyright (c) 2015年 鈴木大貴. All rights reserved.
+//
+
+import UIKit
+
+class SASecretCommandViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

6fe5a21d0afcd2c622df8329431d52e87c8cc3e0
Taiki Suzuki <s1180183@gmail.com>
2015-02-07 00:05:14 +0900

    clean up

--- Example/Tests/Tests.m
+++
@@ -1,9 +0,0 @@
-//
-//  SASecretCommandViewControllerTests.m
-//  SASecretCommandViewControllerTests
-//
-//  Created by Taiki Suzuki on 02/07/2015.
-//  Copyright (c) 2014 Taiki Suzuki. All rights reserved.
-//
-
-${TEST_EXAMPLE}
